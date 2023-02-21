e85d7fc54baeb04fd349edab96537a97414c52fe
mukulembezewil <wilmukulembeze@gmail.com>
2023-02-21 04:20:02 +0300

    GREEN: Finalt test-last commit

--- test/testfactorial.js
+++ test/testfactorial.js
@@ -2,14 +2,18 @@
 const { assert } = require("chai");
 const factorial = require("../factorial");
 
-describe("Factorial unit test", () => {
+describe("Factorial test-last unit test", () => {
   // eslint-disable-next-line prettier/prettier
   
-  it("Factorial test", () => {
+  it("Factorial test-last one", () => {
     assert.equal(factorial(3), 6);
   });
 
-  it("Factorial test", () => {
+  it("Factorial test-last two", () => {
     assert.equal(factorial(7), 7 * 6 * 5 * 4 * 3 * 2 * 1);
+  });
+
+  it("Factorial test-last final", () => {
+    assert.equal(factorial(5), 120);
   });
 });
